319a98f3c2e479b62d6076e616b615e732fa1444
Dave Corley <smokydave@icloud.com>
2025-03-06 18:42:58 -0700

    CLEANUP: Disable unnecessary UI plugins
    TokyoNight, Astrotheme, mini.animate

--- lua/plugins/2-ui.lua
+++ lua/plugins/2-ui.lua
@@ -28,28 +28,28 @@ return {
 
   --  tokyonight [theme]
   --  https://github.com/folke/tokyonight.nvim
-  {
-    "folke/tokyonight.nvim",
-    event = "User LoadColorSchemes",
-    opts = {
-      dim_inactive = false,
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-      },
-    }
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   event = "User LoadColorSchemes",
+  --   opts = {
+  --     dim_inactive = false,
+  --     styles = {
+  --       comments = { italic = true },
+  --       keywords = { italic = true },
+  --     },
+  --   }
+  -- },
 
   --  astrotheme [theme]
   --  https://github.com/AstroNvim/astrotheme
-  {
-    "AstroNvim/astrotheme",
-    event = "User LoadColorSchemes",
-    opts = {
-      palette = "astrodark",
-      plugins = { ["dashboard-nvim"] = true },
-    },
-  },
+  -- {
+  --   "AstroNvim/astrotheme",
+  --   event = "User LoadColorSchemes",
+  --   opts = {
+  --     palette = "astrodark",
+  --     plugins = { ["dashboard-nvim"] = true },
+  --   },
+  -- },
 
   --  alpha-nvim [greeter]
   --  https://github.com/goolord/alpha-nvim
@@ -631,46 +631,46 @@ return {
   --  https://github.com/echasnovski/mini.animate
   --  HINT: if one of your personal keymappings fail due to mini.animate, try to
   --        disable it during the keybinding using vim.g.minianimate_disable = true
-  {
-    "echasnovski/mini.animate",
-    event = "User BaseFile",
-    enabled = not is_android,
-    opts = function()
-      -- don't use animate when scrolling with the mouse
-      local mouse_scrolled = false
-      for _, scroll in ipairs { "Up", "Down" } do
-        local key = "<ScrollWheel" .. scroll .. ">"
-        vim.keymap.set({ "", "i" }, key, function()
-          mouse_scrolled = true
-          return key
-        end, { expr = true })
-      end
-
-      local animate = require("mini.animate")
-      return {
-        open = { enable = false }, -- true causes issues on nvim-spectre
-        resize = {
-          timing = animate.gen_timing.linear { duration = 33, unit = "total" },
-        },
-        scroll = {
-          timing = animate.gen_timing.linear { duration = 50, unit = "total" },
-          subscroll = animate.gen_subscroll.equal {
-            predicate = function(total_scroll)
-              if mouse_scrolled then
-                mouse_scrolled = false
-                return false
-              end
-              return total_scroll > 1
-            end,
-          },
-        },
-        cursor = {
-          enable = false, -- We don't want cursor ghosting
-          timing = animate.gen_timing.linear { duration = 26, unit = "total" },
-        },
-      }
-    end,
-  },
+  -- {
+  --   "echasnovski/mini.animate",
+  --   event = "User BaseFile",
+  --   enabled = not is_android,
+  --   opts = function()
+  --     -- don't use animate when scrolling with the mouse
+  --     local mouse_scrolled = false
+  --     for _, scroll in ipairs { "Up", "Down" } do
+  --       local key = "<ScrollWheel" .. scroll .. ">"
+  --       vim.keymap.set({ "", "i" }, key, function()
+  --         mouse_scrolled = true
+  --         return key
+  --       end, { expr = true })
+  --     end
+  --
+  --     local animate = require("mini.animate")
+  --     return {
+  --       open = { enable = false }, -- true causes issues on nvim-spectre
+  --       resize = {
+  --         timing = animate.gen_timing.linear { duration = 33, unit = "total" },
+  --       },
+  --       scroll = {
+  --         timing = animate.gen_timing.linear { duration = 50, unit = "total" },
+  --         subscroll = animate.gen_subscroll.equal {
+  --           predicate = function(total_scroll)
+  --             if mouse_scrolled then
+  --               mouse_scrolled = false
+  --               return false
+  --             end
+  --             return total_scroll > 1
+  --           end,
+  --         },
+  --       },
+  --       cursor = {
+  --         enable = false, -- We don't want cursor ghosting
+  --         timing = animate.gen_timing.linear { duration = 26, unit = "total" },
+  --       },
+  --     }
+  --   end,
+  -- },
 
   --  highlight-undo
   --  https://github.com/tzachar/highlight-undo.nvim
